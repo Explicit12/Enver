427b7de16055fa3fb711e4bf23e41444b8a3a89b
Explicit12 <explicit1723@gmail.com>
2022-06-15 17:12:13 +0300

    Removed links to empty; Deleted unnecessary async functions.

--- src/js/loader.js
+++ src/js/loader.js
@@ -4,13 +4,13 @@ export default class PageLoader {
     static #loaderDOM = document.querySelector(".loader-cover");
     static #htmlElem = document.querySelector("html");
 
-    static async init() {
+    static init() {
         window.addEventListener("load", this.#removeLoader.bind(this));
         this.#htmlElem.style.overflowY = "hidden";
         return this;
     }
 
-    static async #removeLoader() {
+    static #removeLoader() {
         Animate.hide(this.#loaderDOM, 3500);
         this.#htmlElem.style.overflowY = "visible";
         this.#loaderDOM.remove();
